1db6810602c5e22568ff8526fd7634c079ec0207
Valentin <valentingaudin@gmail.com>
2021-12-05 22:58:43 +0100

    Change the httpEquiv

--- components/Meta.js
+++ components/Meta.js
@@ -41,7 +41,7 @@ export default function Meta() {
             <meta name="msapplication-TileColor" content="#0e1212" />
             <meta name="theme-color" content="#0e1212" />
             <meta charset="UTF-8" />
-            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <meta name="description" content="My portfolio about me and my work on web devlopment using Next.js " />
             <title>Valentin Gaudin</title>
