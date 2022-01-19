5da92bde530797befdcc1c5bf826c6918463e674
Valentin <valentingaudin@gmail.com>
2022-01-19 10:22:40 +0100

    Difficultes with a head element with W3C validator

--- components/Meta.js
+++ components/Meta.js
@@ -1,9 +1,8 @@
-import Head from 'next/head'
 import { DESCRIPTION, NAME } from '../lib/constant'
 
 export default function Meta() {
     return (
-        <Head>
+        <>
             <link
                 rel="apple-touch-icon"
                 sizes="180x180"
@@ -43,6 +42,6 @@ export default function Meta() {
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <meta name="description" content={DESCRIPTION} />
             <title>{NAME}</title>
-        </Head >
+        </>
     )
 }
